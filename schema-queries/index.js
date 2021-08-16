eb0205c419bd29c92543ab5d78859848a85126f0
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2021-08-15 22:18:33 -0300

    Added type Product, query, resolver

--- schema-queries/index.js
+++ schema-queries/index.js
@@ -11,11 +11,19 @@ const typeDefs = gql`
         salary: Float
         vip: Boolean
     }
+
+    type Product {
+        name: String!
+        price: Float!
+        discount: Float
+        priceWithDiscount: Float
+    }
     
     type Query {
         hello: String
         date: Date
         loggedUser: User
+        mainProduct: Product
     }
 `
 
@@ -23,6 +31,12 @@ const resolvers = {
     User: {
       salary(user) {
           return user.salary_brute
+      },
+    },
+    Product: {
+      priceWithDiscount(product) {
+          if (!product.discount) return product.price
+          return product.price * (1 - product.discount)
       },
     },
     Query: {
@@ -35,11 +49,18 @@ const resolvers = {
         loggedUser() {
             return {
                 id: 1,
-                name: 'Lourivaldo',
+                name: 'Lourivaldo Vasconcelos',
                 email: 'lourivaldovasconcelos@gamil.com',
                 age: 20,
                 salary_brute: 1234.56,
                 vip: true
+            }
+        },
+        mainProduct() {
+            return {
+                name: 'Macbook',
+                price: 12000,
+                discount: 0.1,
             }
         }
     }

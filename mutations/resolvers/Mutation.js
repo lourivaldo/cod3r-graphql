6ae0d20cd22adf34c8ea87cb4f7451c7390349d8
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2021-08-22 22:11:07 -0300

    Added create,update,remove user mutations

--- mutations/resolvers/Mutation.js
+++ mutations/resolvers/Mutation.js
@@ -16,5 +16,23 @@ module.exports = {
         }
         users.push(user)
         return user
+    },
+    removeUser(_, { id }) {
+        const index = users.findIndex(u => u.id === id)
+        if (index < 0) return null
+        const removedUsers = users.splice(index, 1)
+        return removedUsers ? removedUsers[0] : null
+    },
+    updateUser(_, { id, name, email, age }) {
+        const index = users.findIndex(u => u.id === id)
+        if (index < 0) return null
+        const user = {
+            ...users[index],
+            name,
+            email,
+            age
+        }
+        users.splice(index, 1, user)
+        return user
     }
 }

e6979ddbedbb9c04abf2f941551c9763ddddbcf8
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2021-08-28 19:36:10 -0300

    Added database project + knex + docker

---
+++ database/migrations/20210828222312_create_roles_table.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('roles', table => {
+        table.increments('id').primary()
+        table.string('name').notNull().unique()
+        table.string('label').notNull()
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('roles')
+};

24fd8e032c2ab54256cef2eda3062c9dd6d2857a
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2021-08-29 10:33:08 -0300

    Added migrations

--- database/knexfile.js
+++ database/knexfile.js
@@ -1,8 +1,12 @@
-const { connection } = require('./.env');
+require('dotenv').config()
 
 module.exports = {
     client: 'mysql',
-    connection,
+    connection: {
+        database: process.env.DB_DATABASE,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+    },
     pool: {
         min: 2,
         max: 10
--- database/migrations/20210828222425_create_roles_users_table.js
+++ database/migrations/20210828222425_create_roles_users_table.js
@@ -1,7 +1,14 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('roles_users', table => {
+        table.integer('user_id').unsigned()
+        table.integer('role_id').unsigned()
+        table.foreign('user_id').references('users.id')
+        table.foreign('role_id').references('roles.id')
+        table.primary(['user_id', 'role_id'])
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('roles_users');
 };

a908baf21214050e52d5e454a5dd2b5f21955990
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2021-09-06 10:55:12 -0300

    Added users search

--- final-project/frontend/src/componentes/user/Search.vue
+++ final-project/frontend/src/componentes/user/Search.vue
@@ -1,67 +1,94 @@
 <template>
-    <v-container fluid>
-        <v-layout>
-            <v-flex>
-                <v-layout column class="ma-3">
-                    <h1 class="headline">Consultar Usuário</h1>
-                    <v-divider class="mb-3" />
-                        <div v-if="erros">
-                            <Erros :erros="erros" />
-                        </div>
-                        <v-text-field label="ID"
-                            v-model.number="filtro.id" />
-                        <v-text-field label="E-mail"
-                            v-model="filtro.email" />
-                        <v-btn color="primary" class="ml-0 mt-3"
-                            @click="search">
-                            Consultar
-                        </v-btn>
-                </v-layout>
-            </v-flex>
-            <v-flex>
-                <v-layout column class="ma-3">
-                    <h1 class="headline">Resultado</h1>
-                    <v-divider />
-                    <template v-if="dados">
-                        <v-text-field label="ID" readonly
-                            v-model="dados.id" />
-                        <v-text-field label="Nome" readonly
-                            v-model="dados.nome" />
-                        <v-text-field label="E-mail" readonly
-                            v-model="dados.email" />
-                        <v-text-field label="Perfis" readonly
-                            :value="roleLabels" />
-                    </template>
-                </v-layout>
-            </v-flex>
+  <v-container fluid>
+    <v-layout>
+      <v-flex>
+        <v-layout column class="ma-3">
+          <h1 class="headline">Consultar Usuário</h1>
+          <v-divider class="mb-3"/>
+          <div v-if="errors">
+            <Errors :errors="errors"/>
+          </div>
+          <v-text-field label="ID"
+                        v-model.number="filter.id"/>
+          <v-text-field label="E-mail"
+                        v-model="filter.email"/>
+          <v-btn color="primary" class="ml-0 mt-3"
+                 @click="search">
+            Consultar
+          </v-btn>
         </v-layout>
-    </v-container>
+      </v-flex>
+      <v-flex>
+        <v-layout column class="ma-3">
+          <h1 class="headline">Resultado</h1>
+          <v-divider/>
+          <template v-if="data">
+            <v-text-field label="ID" readonly
+                          v-model="data.id"/>
+            <v-text-field label="Nome" readonly
+                          v-model="data.name"/>
+            <v-text-field label="E-mail" readonly
+                          v-model="data.email"/>
+            <v-text-field label="Perfis" readonly
+                          :value="roleLabels"/>
+          </template>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import Erros from '../shared/Erros'
+import gql from 'graphql-tag'
+import Errors from '../shared/Erros'
 
 export default {
-    components: { Erros },
-    data() {
-        return {
-            filtro: {},
-            roles: [],
-            dados: null,
-            erros: null
-        }
-    },
-    computed: {
-        roleLabels() {
-            return this.dados && this.dados.roles &&
-                this.dados.roles.map(p => p.rotulo).join(', ')
-        }
-    },
-    methods: {
-        search() {
-            // implementar
-        }
+  components: {
+    Errors
+  },
+  data() {
+    return {
+      filter: {},
+      roles: [],
+      data: null,
+      errors: null
     }
+  },
+  computed: {
+    roleLabels() {
+      return this.data && this.data.roles &&
+          this.data.roles.map(p => p.label).join(', ')
+    }
+  },
+  methods: {
+    search() {
+      this.$api.query({
+        query: gql`
+          query Query(
+            $id: Int
+            $email: String
+          ) {
+            user(filter: {
+                id: $id 
+                email: $email
+            }) { id name email roles { label } }
+          }
+        `,
+        variables: {
+          id: this.filter.id,
+          email: this.filter.email,
+        },
+        fetchPolicy: 'network-only',
+      }).then((result) => {
+        this.data = result.data.user
+        this.filter = {}
+        this.errors = null
+      }).catch(err => {
+        this.data = null
+        this.errors = err
+      })
+    }
+  }
 }
 </script>
 
